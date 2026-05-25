a5b1f9bb2a309308f8f94bf8185078b533907d71
Suhani Nagpal <suhaninagpal95@gmail.com>
2026-05-25 15:42:24 +0530

    final deploy fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,17 +33,13 @@ function App() {
       setLoading(true);
 
       const res = await axios.post(
-
-        "https://ai-resume-analyzer-2-1ne2.onrender.com/api/resume/upload",
-
+        `${import.meta.env.VITE_API_URL}/api/resume/upload`,
         formData,
-
         {
           headers: {
             "Content-Type": "multipart/form-data",
           },
         }
-
       );
 
       setMessage(res.data.message);
@@ -59,11 +55,8 @@ function App() {
       setMessage("Upload failed");
 
     } finally {
-
       setLoading(false);
-
     }
-
   };
 
   return (
@@ -89,95 +82,69 @@ function App() {
           onClick={handleUpload}
           className="bg-blue-600 px-6 py-3 rounded-lg hover:bg-blue-700 w-full text-lg font-semibold"
         >
-
           {loading ? "Uploading..." : "Upload Resume"}
-
         </button>
 
         {message && (
-
           <p className="text-green-400 font-semibold text-center">
             {message}
           </p>
-
         )}
 
       </div>
 
       {/* ATS Score */}
       {atsScore > 0 && (
-
         <div className="bg-green-700 px-8 py-5 rounded-2xl mt-8 shadow-lg">
-
           <h2 className="text-3xl font-bold text-center">
             ATS Score: {atsScore}/100
           </h2>
-
         </div>
-
       )}
 
       {/* Detected Skills */}
       {skills.length > 0 && (
-
         <div className="bg-gray-900 p-6 rounded-2xl mt-8 w-[700px] shadow-lg">
-
           <h2 className="text-3xl font-bold mb-6 text-center">
             Detected Skills
           </h2>
 
           <div className="flex flex-wrap gap-4 justify-center">
-
             {skills.map((skill, index) => (
-
               <div
                 key={index}
                 className="bg-blue-600 px-5 py-2 rounded-full text-lg font-medium"
               >
                 {skill}
               </div>
-
             ))}
-
           </div>
-
         </div>
-
       )}
 
       {/* Missing Skills */}
       {missingSkills.length > 0 && (
-
         <div className="bg-gray-900 p-6 rounded-2xl mt-8 w-[700px] shadow-lg">
-
           <h2 className="text-3xl font-bold mb-6 text-center">
             Missing Skills
           </h2>
 
           <div className="flex flex-wrap gap-4 justify-center">
-
             {missingSkills.map((skill, index) => (
-
               <div
                 key={index}
                 className="bg-red-600 px-5 py-2 rounded-full text-lg font-medium"
               >
                 {skill}
               </div>
-
             ))}
-
           </div>
-
         </div>
-
       )}
 
       {/* AI Suggestions */}
       {aiSuggestions && (
-
         <div className="bg-gray-900 p-6 rounded-2xl mt-8 w-[700px] shadow-lg">
-
           <h2 className="text-3xl font-bold mb-6 text-center">
             AI Suggestions
           </h2>
@@ -185,36 +152,26 @@ function App() {
           <pre className="whitespace-pre-wrap leading-8 text-gray-300 text-base">
             {aiSuggestions}
           </pre>
-
         </div>
-
       )}
 
       {/* Resume Text */}
       {resumeText && (
-
         <div className="bg-gray-900 p-6 rounded-2xl mt-8 w-[700px] shadow-lg">
-
           <h2 className="text-3xl font-bold mb-6 text-center">
             Extracted Resume Text
           </h2>
 
           <div className="bg-black p-5 rounded-xl max-h-[400px] overflow-y-auto">
-
             <pre className="text-sm whitespace-pre-wrap leading-7 text-gray-300">
               {resumeText}
             </pre>
-
           </div>
-
         </div>
-
       )}
 
     </div>
-
   );
-
 }
 
 export default App;
